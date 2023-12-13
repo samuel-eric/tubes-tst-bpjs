54f3d35a19279642698daa70019318a3409aed69
samuel-eric <samueltan086@gmail.com>
2023-12-13 14:07:29 +0700

    selesai halaman about

--- app/Views/about.php
+++ app/Views/about.php
@@ -1,3 +1,12 @@
 <div class="main container">
-  <h1>This is about</h1>
+  <div class="about-img-container">
+    <img src="/home-img.jpg" alt="Dokter" class="about-img">
+  </div>
+    <div class="about-text-container">
+      <h1>BPJS TST</h1>
+      <h3>Hanya tugas besar mata kuliah TST</h3>
+      <p>Ini adalah website untuk tugas besar mata <br> kuliah Teknologi Sistem Terintegrasi.</p>
+      <p>Credit for image goes to: </p>
+      <p>Bagoes Ilhamy | Unsplash | <a href="https://unsplash.com/photos/a-man-laying-in-a-hospital-bed-being-examined-by-a-nurse-Pi_H3N_qbVQ?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash" style="color: inherit" target="_blank">Link</a></p>
+    </div>
 </div>

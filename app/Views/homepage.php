6782995f357992381d47b7990816b217afd8a645
samuel-eric <samueltan086@gmail.com>
2023-12-13 13:54:38 +0700

    selesai laman home

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -1,3 +1,11 @@
 <div class="main container">
-  <h1>This is the home</h1>
+  <div class="home-img-container">
+    <img src="/home-img.jpg" alt="Dokter" class="home-img">
+  </div>
+    <div class="home-text-container">
+      <h1>BPJS TST</h1>
+      <h3>Daftarkan diri Anda sekarang!</h3>
+      <p>Pelayanan mudah, cepat, dan simpel</p>
+      <a href="/daftar" class="cta-btn">Daftar sekarang!</a>
+    </div>
 </div>

a69f87ba20069ef8631be40f890a7884a50f7fa8
samuel-eric <samueltan086@gmail.com>
2023-12-13 21:43:56 +0700

    tambah tampilan daftar bpjs

--- app/Views/daftar.php
+++ app/Views/daftar.php
@@ -1,3 +1,19 @@
-<div class="main">
-  <h1>Ini tempat pendaftaran</h1>
+<div class="main flex-main">
+  <div class="card daftar">
+    <h1>Daftar</h1>
+    <form action="/signup_action" method="POST">
+      <input type="text" name="nama" placeholder="Nama Lengkap" required>
+      <input type="text" name="nik" placeholder="NIK" required>
+      <input type="text" name="alamat" placeholder="Alamat" required>
+      <input type="text" onfocus="(this.type='date')" onblur="(this.type='text')" name="tanggal_lahir" placeholder="Tanggal Lahir" required>
+      <select name="jenis_kelamin" required>
+        <option value="">Pilih Jenis Kelamin</option>
+        <option value="M">Laki-laki</option>
+        <option value="F">Perempuan</option>
+      </select>
+      <input type="text" name="no_telp" placeholder="Nomor Telepon" required>
+      <input type="password" name="password" placeholder="Password" required>
+      <button type="submit">Daftar</button>
+    </form>
+  </div>
 </div>
--- app/Views/login.php
+++ app/Views/login.php
@@ -2,8 +2,8 @@
   <div class="card login">
     <h1>Login</h1>
     <form action="/login_action" method="POST">
-      <input type="text" name="no_kartu" placeholder="Nomor Kartu BPJS">
-      <input type="password" name="password" placeholder="Password">
+      <input type="text" name="no_kartu" placeholder="Nomor Kartu BPJS" required>
+      <input type="password" name="password" placeholder="Password" required>
       <button type="submit">Masuk</button>
     </form>
   </div>

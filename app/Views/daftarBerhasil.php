b50c9197405ba20b3900298c3f4c2351b6d1aa1c
samuel-eric <samueltan086@gmail.com>
2023-12-14 08:52:15 +0700

    hiasin tampilan berhasil daftar

--- app/Views/daftarBerhasil.php
+++ app/Views/daftarBerhasil.php
@@ -1,5 +1,8 @@
 <div class="main flex-main">
-  <h1>Data Berhasil Terdaftar</h1>
-  <p>Berikut adalah nomor kartu BPJS Anda:</p>
-  <p><?= $no_kartu ?></p>
+  <div class="card daftar-berhasil">
+    <h1>Data Berhasil Terdaftar</h1>
+    <p>Berikut adalah nomor kartu BPJS Anda:</p>
+    <p><strong><?= $no_kartu ?></strong></p>
+    <p>Silakan login</p>
+  </div>
 </div>

aeff746416c588862c9c1ef0ff4b04ddbc19f762
samuel-eric <samueltan086@gmail.com>
2023-12-14 14:52:17 +0700

    tambah favicon

--- app/Views/htmlTemplate.php
+++ app/Views/htmlTemplate.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>BPJS Kesehatan</title>
-  <!-- Tambahin favicon -->
+  <title>BPJS TST</title>
+  <link rel="shortcut icon" type="image/png" href="/favicon.ico">
   <link rel="stylesheet" href="/style.css">
 </head>
 <body>
